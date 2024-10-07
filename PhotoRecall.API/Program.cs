06642c93450fe569532f68da40f1c115f1cccfef
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-10-07 20:00:42 +0200

    Add Newtonsoft.Json package

--- PhotoRecall.API/Program.cs
+++ PhotoRecall.API/Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Inject Services
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IPredictionsService, PredictionsService>();
 
