2b567230a63776b1594a65f57c5afa767d269cf8
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-10-27 17:31:16 +0100

    Create functioning code for getting all predictions from all available models.

--- PhotoRecall.API/Predictions/IPredictionsService.cs
+++ PhotoRecall.API/Predictions/IPredictionsService.cs
@@ -1,6 +1,10 @@
+using Data;
+using Data.Dtos;
+
 namespace PhotoRecall.API.Predictions;
 
 public interface IPredictionsService
 {
-    Task<List<PredictionDto>> GetPredictionsAsync();
+    Task<List<PredictionDto>> GetVotedPredictionsAsync(List<YoloRunnerConfig> yoloRunnersConfig);
+    Task<List<YoloRunnerResultDto>> GetAllPredictionsAsync(List<YoloRunnerConfig> yoloRunnersConfig);
 }
--- PhotoRecall.API/Predictions/PredictionsService.cs
+++ PhotoRecall.API/Predictions/PredictionsService.cs
@@ -1,7 +1,11 @@
 using System.Net.Http;
+using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.Serialization;
+using Data;
+using Data.Dtos;
+using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
-using PhotoRecall.API.Configuration;
+using PredictionUtils;
 
 namespace PhotoRecall.API.Predictions;
 
@@ -9,66 +13,115 @@ public class PredictionsService : IPredictionsService
 {
     private static readonly HttpClient Client = new HttpClient();
     
-    public async Task<List<PredictionDto>> GetPredictionsAsync()
+    public async Task<List<PredictionDto>> GetVotedPredictionsAsync(List<YoloRunnerConfig> yoloRunnersConfig)
     {
-        var result = new List<PredictionDto>();
-        
-        var yoloModulesConfig = GetYoloModulesConfiguration();
+        ValidateYoloRunnersConfig(yoloRunnersConfig);
 
-        if (yoloModulesConfig == null)
+        var predictions = await RunYoloRunners(yoloRunnersConfig);
+
+        return PredictionsProcessor.MergeByVoting(predictions);
+    }
+    
+    public async Task<List<YoloRunnerResultDto>> GetAllPredictionsAsync(List<YoloRunnerConfig> yoloRunnersConfig)
+    {
+        ValidateYoloRunnersConfig(yoloRunnersConfig);
+
+        return await RunYoloRunners(yoloRunnersConfig);
+    }
+
+    #region ValidateYoloRunnersConfig
+
+    private static void ValidateYoloRunnersConfig(List<YoloRunnerConfig> yoloRunnersConfig)
+    {
+        if (yoloRunnersConfig is not { Count: > 0 })
         {
-            throw new Exception("Could not read yolo modules configuration.");
+            throw new Exception("YoloRunners configuration is empty.");
         }
 
-        await Parallel.ForEachAsync(yoloModulesConfig, async (config, cancellationToken) =>
+        if (yoloRunnersConfig.Any(a => a.Addresses?.Count == 0))
         {
-            var singleResult = await GetResultsFromYoloModel(config, cancellationToken);
-            
-            lock (result)
-            {
-                result.AddRange(singleResult);
-            }
-        });
+            throw new Exception("One or more YoloRunners have invalid url configuration.");
+        }
+    }
+
+    #endregion
+    
+    private static async Task<List<YoloRunnerResultDto>> RunYoloRunners(List<YoloRunnerConfig> yoloRunnersConfig)
+    {
+        var result = new List<YoloRunnerResultDto>();
         
+        await Parallel.ForEachAsync(yoloRunnersConfig, 
+            async (yoloRunner, cancellationToken) =>
+        {
+            var modelsToRun = yoloRunner.Models.ConvertAll(model => model);
+            
+            await Parallel.ForEachAsync(yoloRunner.Addresses, cancellationToken, 
+                async (address, cancellationToken) =>
+            {
+                while (modelsToRun.Count > 0)
+                {
+                    string model;
+                    lock (modelsToRun)
+                    {
+                        var modelNullable = modelsToRun.FirstOrDefault();
+
+                        if (modelNullable == null)
+                            continue;
+                        
+                        model = modelNullable;
+                        modelsToRun.RemoveAt(0);
+                    }
+                    
+                    var yoloModelPredictions = new YoloRunnerResultDto()
+                    {
+                        YoloRunnerExecution = new YoloRunnerExecutionDto()
+                        {
+                            Name = yoloRunner.Name,
+                            Address = address,
+                            Model = model
+                        },
+                        Predictions = await GetResultsFromYoloModel(address, model, cancellationToken)
+                    };
+                
+                    lock (result)
+                    {
+                        result.Add(yoloModelPredictions);
+                    }
+                }
+            });
+        });
+
         return result;
     }
 
-    private async Task<List<PredictionDto>> GetResultsFromYoloModel(YoloModuleConfiguration moduleConfig, 
+    private static async Task<List<PredictionDto>> GetResultsFromYoloModel(string address, string model, 
         CancellationToken cancellationToken)
     {
-        var uri = BuildRequestUri(moduleConfig.Url, "https://ultralytics.com/images/bus.jpg");
-            
-        var response = await Client.GetStringAsync(uri, cancellationToken);
-        if (response == string.Empty)
+        var uri = $"http://{address}/predict";
+        
+        var queryParams = new Dictionary<string, string?>
         {
-            throw new HttpRequestException("No response");
+            { "photo_url", "https://ultralytics.com/images/bus.jpg" },
+            { "model_name", model }
+        };
+
+        var finalUri = new Uri(QueryHelpers.AddQueryString(uri, queryParams));
+        
+        var response = await Client.GetAsync(finalUri, cancellationToken);
+        var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+        if (!response.IsSuccessStatusCode || responseContent == string.Empty)
+        {
+            // Log
+            return [];
         }
             
-        var result = JsonConvert.DeserializeObject<List<PredictionDto>>(response);
+        var result = JsonConvert.DeserializeObject<List<PredictionDto>>(responseContent);
         if (result is null)
         {
-            throw new SerializationException();
+            // Log
+            return [];
         }
 
         return result;
-    }
-
-    private List<YoloModuleConfiguration> GetYoloModulesConfiguration()
-    {
-        var result = new List<YoloModuleConfiguration>();
-        
-        IConfigurationRoot config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .AddEnvironmentVariables()
-            .Build();
-        
-        result = config.GetRequiredSection("YoloModules").Get<List<YoloModuleConfiguration>>();
-        
-        return result;
-    }
-
-    private string BuildRequestUri(string moduleUrl, string photoUrl)
-    {
-        return $"{moduleUrl}/predict?photo_url={photoUrl}";
     }
 }
