7fccee78d2a3adedaaf5800ebaa175321f7ae248
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-10-27 17:26:16 +0100

    Create PredictionsProcessor

---
+++ PredictionUtils/PredictionsProcessor.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using Data.Dtos;
+
+namespace PredictionUtils;
+
+public static class PredictionsProcessor
+{
+    public static List<PredictionDto> MergeByVoting(List<YoloRunnerResultDto> predictions)
+    {
+        var result = new List<PredictionDto>();
+        
+        if (predictions.Count <= 0)
+        {
+            return result;
+        } 
+        
+        var firstPrediction = predictions.First();
+        
+        // foreach (var yoloRunnerName in predictions.Keys.Take(new Range(1, Index.End)))
+        // {
+        //     
+        // }
+
+        return result;
+    }
+}
