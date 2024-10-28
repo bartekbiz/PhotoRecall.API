714e4cbc6d21a9a09ed90c89df04921aafa99e64
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-10-28 13:41:43 +0100

    Create file utils with method for saving files received from endpoint

---
+++ Utils/FileUtils.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Utils;
+
+public static class FileUtils
+{
+    public static async Task<string> SaveFormFile(string dir, IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            throw new Exception("File is null!");
+        }
+        
+        var newFileName = $"{new Guid()}.{file.FileName.Split(".").ToList().Last()}";
+        
+        var photosPath = Path.Combine(Directory.GetCurrentDirectory(), dir);
+        if (!Directory.Exists(photosPath))
+        {
+            Directory.CreateDirectory(photosPath);
+        }
+        
+        var path = Path.Combine(photosPath, newFileName);
+        
+        await using var stream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        if (!File.Exists(path))
+        {
+            throw new Exception("Could not save file!");
+        }
+        
+        return path;
+    }
+
+    public static void ClearDirectory(string path)
+    {
+        
+    }
+}
