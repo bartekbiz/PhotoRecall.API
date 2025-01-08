6ec1825d3cddbce324c5ccc0ff95127b4f7cdb93
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2025-01-08 15:01:34 +0100

    Clean up code, adjust to changes in PredictionMergers

--- PhotoRecall.API/Predictions/PredictionsService.cs
+++ PhotoRecall.API/Predictions/PredictionsService.cs
@@ -1,7 +1,6 @@
 using Data;
 using Data.Dtos;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using PhotoRecall.API.Exceptions;
 using PhotoRecall.API.Info;
 using Utils;
@@ -13,29 +12,104 @@ public class PredictionsService : IPredictionsService
 {
     private readonly ILogger<PredictionsService> _logger;
     private readonly IInfoService _infoService;
-    private readonly List<YoloRunnerConfig> _yoloRunnersConfig;
+    private readonly List<ModelRunnerConfig> _modelRunnersConfig;
     private readonly PhotosConfig _photosConfig;
     private readonly UrlsConfig _urlsConfig;
     private readonly PredictionsGetter _predictionsGetter;
 
     public PredictionsService(ILogger<PredictionsService> logger,
         IInfoService infoService,
-        IOptions<List<YoloRunnerConfig>> yoloRunnersConfig,
+        IOptions<List<ModelRunnerConfig>> yoloRunnersConfig,
         IOptions<PhotosConfig> photosConfig,
         IOptions<UrlsConfig> urlsConfig)
     {
         _logger = logger;
         _infoService = infoService;
-        _yoloRunnersConfig = yoloRunnersConfig.Value;
-        ValidateYoloRunnersConfig(_yoloRunnersConfig);
+        _modelRunnersConfig = yoloRunnersConfig.Value;
+        ValidateYoloRunnersConfig(_modelRunnersConfig);
         _photosConfig = photosConfig.Value;
         _urlsConfig = urlsConfig.Value;
-        _predictionsGetter = new PredictionsGetter(logger, _yoloRunnersConfig);
+        _predictionsGetter = new PredictionsGetter(logger, _modelRunnersConfig);
     }
+    
+    #region GetMergedPredictions
+   
+    public async Task<List<PredictionDtoMerged>> GetPredictionsAllDetectedAsync(PredictionPropsDto propsDto)
+    {
+        var modelCount = GetModelsList(propsDto).Count;
+        var threshold = (propsDto.AgreeRatio / 100) * modelCount;
+
+        return await GetMergedPredictions(PredictionsMergerType.AllDetected, propsDto, threshold);
+    }
+    
+    public async Task<List<PredictionDtoMerged>> GetVotedPredictionsWithCountAsync(PredictionPropsDto propsDto)
+    {
+        return await GetMergedPredictions(PredictionsMergerType.VotedWithCounts, propsDto, new object());
+    }
+    
+    private async Task<List<PredictionDtoMerged>> GetMergedPredictions(PredictionsMergerType mergerType, 
+        PredictionPropsDto propsDto, object args)
+    {
+        var predictions = await GetPredictionsAsync(propsDto);
+
+        var predictionsMerger = PredictionsMergerFactory.Create(mergerType);
+        
+        try
+        {
+            if (predictionsMerger == null)
+            {
+                throw new Exception();
+            }
+            
+            return predictionsMerger.Merge(predictions, args);
+        }
+        catch
+        {
+            throw new Exception("Could not merge predictions :(");
+        }
+    }
+    
+    #endregion
+    
+    #region GetPredictions
+    
+    public async Task<List<ModelRunResultDto>> GetAllPredictionsAsync(IFormFile photo)
+    {
+        var modelsList = _infoService.GetAvailableYoloModels();
+        ValidatePhoto(photo);
+        
+        return await GetPredictionsAsync(photo, modelsList);
+    }
+    
+    public async Task<List<ModelRunResultDto>> GetPredictionsAsync(PredictionPropsDto propsDto)
+    {
+        var modelsList = GetModelsList(propsDto);
+        
+        ValidateProps(propsDto.Photo, modelsList);
+
+        return await GetPredictionsAsync(propsDto.Photo, modelsList!);
+    }
+
+    private async Task<List<ModelRunResultDto>> GetPredictionsAsync(IFormFile photo, List<string> modelsList)
+    {
+        var hostedPhoto = await FileUtils
+            .SaveAndHostFile(_photosConfig.Path, _urlsConfig.ContainerUrl, photo);
+
+        var predictions = await _predictionsGetter
+            .GetPredictions(hostedPhoto.Uri, modelsList);
+
+        FileUtils.DeleteFile(hostedPhoto.Path);
+
+        return predictions;
+    }
+    
+    #endregion
+    
+    #region Utils
     
     #region ValidateYoloRunnerConfig
 
-    private void ValidateYoloRunnersConfig(List<YoloRunnerConfig> yoloRunnersConfig)
+    private void ValidateYoloRunnersConfig(List<ModelRunnerConfig> yoloRunnersConfig)
     {
         if (yoloRunnersConfig is not { Count: > 0 })
         {
@@ -55,70 +129,6 @@ public class PredictionsService : IPredictionsService
 
     #endregion
     
-    public async Task<List<YoloRunResultDto>> GetAllPredictionsAsync(IFormFile photo)
-    {
-        var modelsList = _infoService.GetAvailableYoloModels();
-        ValidatePhoto(photo);
-        
-        return await GetPredictionsAsync(photo, modelsList);
-    }
-   
-    public async Task<List<PredictionDtoBase>> GetPredictionsAllDetectedAsync(PredictionPropsDto propsDto)
-    {
-        var modelCount = GetModelsList(propsDto).Count;
-        var threshold = (propsDto.AgreeRatio / 100) * modelCount;
-
-        return await GetMergedPredictions<PredictionDtoBase>(propsDto, threshold);
-    }
-    
-    public async Task<List<PredictionWithCountDto>> GetVotedPredictionsWithCountAsync(PredictionPropsDto propsDto)
-    {
-        return await GetMergedPredictions<PredictionWithCountDto>(propsDto, new object());
-    }
-    
-    private async Task<List<TResult>> GetMergedPredictions<TResult>(PredictionPropsDto propsDto, object args)
-    {
-        var predictions = await GetPredictionsAsync(propsDto);
-
-        var predictionsMerger = PredictionsMergerFactory.Create<TResult>();
-        
-        try
-        {
-            if (predictionsMerger == null)
-            {
-                throw new Exception();
-            }
-            
-            return predictionsMerger.Merge(predictions, args);
-        }
-        catch
-        {
-            throw new Exception("Could not merge predictions :(");
-        }
-    }
-
-    public async Task<List<YoloRunResultDto>> GetPredictionsAsync(PredictionPropsDto propsDto)
-    {
-        var modelsList = GetModelsList(propsDto);
-        
-        ValidateProps(propsDto.Photo, modelsList);
-
-        return await GetPredictionsAsync(propsDto.Photo, modelsList!);
-    }
-
-    private async Task<List<YoloRunResultDto>> GetPredictionsAsync(IFormFile photo, List<string> modelsList)
-    {
-        var hostedPhoto = await FileUtils
-            .SaveAndHostFile(_photosConfig.Path, _urlsConfig.ContainerUrl, photo);
-
-        var predictions = await _predictionsGetter
-            .GetPredictions(hostedPhoto.Uri, modelsList);
-
-        FileUtils.DeleteFile(hostedPhoto.Path);
-
-        return predictions;
-    }
-
     #region ValidateProps
 
     private void ValidateProps(IFormFile photo, List<string>? modelsList)
@@ -141,7 +151,7 @@ public class PredictionsService : IPredictionsService
     }
 
     #endregion
-
+    
     private List<string> GetModelsList(PredictionPropsDto propsDto)
     {
         var modelsListProps = string.IsNullOrEmpty(propsDto.YoloModels) ?
@@ -150,4 +160,6 @@ public class PredictionsService : IPredictionsService
 
         return modelsListProps ?? _infoService.GetAvailableYoloModels();
     }
+
+    #endregion
 }
